69c338fedfdaa15d6bce0299203def47b7a16000
harrisonleggio <harrisonleggio@my.uri.edu>
2017-07-09 22:57:30 -0400

    Working on stuff

--- pepes.js
+++ pepes.js
@@ -4,6 +4,7 @@ TODO:
 
 (1): Compile all images on page and store in array.
 (2): Load list of pepes (static/dynamic???)
+    -- Use reddit api... like this: https://www.reddit.com/r/pepes/hot/.json?count=20
 (3): Match each image with a Pepe of similar size
 (4): Replace images
 
