f7828ed4cc2fdd4290f07170fc3ae330d97c32f2
harrisonleggio <harrisonleggio@my.uri.edu>
2017-07-09 21:29:34 -0400

    Initial file structure

---
+++ pepes.js
@@ -0,0 +1,28 @@
+/*
+
+TODO:
+
+(1): Compile all images on page and store in array.
+(2): Load list of pepes (static/dynamic???)
+(3): Match each image with a Pepe of similar size
+(4): Replace images
+
+*/
+
+var getImages = function(){
+    //code goes here
+}
+
+
+var loadPepes = function(){
+    //code goes here
+}
+
+
+var matchPepes = function(){
+    //code goes here
+}
+
+var replaceImages = function(){
+    //code goes here
+}
